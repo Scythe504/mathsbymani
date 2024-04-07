3c902ae1b74a41f56738b059409448adcd687959
Scythe504 <hellfireprince98@gmail.com>
2024-04-07 22:33:04 +0530

    1.3

--- src/app/app-data/data.ts
+++ src/app/app-data/data.ts
@@ -1,9 +1,8 @@
 export const feature = [
     "Best Mathematics Academic Institute for Class 11 | 12 | Science | Commerce | IIT-JEE.",
-    "Course completion within 4-months of time",
+    "Course completion within 4-months of time and Full Attention given to each student",
     "Start from the bottom and become a Pro at Mathematics.",
     "Classes held for 6days a week.",
     "Study materials, formula sheets, notes and solution are provided.",
     `All lectures are provided on the YouTube Channel`,
-    "Full Attention given to each student"
 ]
