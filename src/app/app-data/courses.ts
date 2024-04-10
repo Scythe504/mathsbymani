8da554789dfa5d40cbeb3f6d783401c840a77f47
Scythe504 <hellfireprince98@gmail.com>
2024-04-10 11:08:00 +0530

    v1.8

--- src/app/app-data/courses.ts
+++ src/app/app-data/courses.ts
@@ -11,24 +11,23 @@ export const courses = [{
     students: "5 Students only",
     fees: "₹10000/- per month only",
     color: `bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
-    from-purple-400 to-purple-600`,
-    text: "text-black"
-},
-{
-    name: "Normal",
-    students: "30-50 Students only",
-    fees: "₹2000/- per month only",
-    color: `bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
-    from-orange-300 to-orange-500`,
-    text: "text-black"
-},
-
-{
-    name: "Platinum",
-    students: "1-2 Students only",
-    fees: "₹20000/- per month only",
-    color: `bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
     from-gray-200 to-slate-500`,
     text: "text-black"
 },
+// {
+//     name: "Normal",
+//     students: "30-50 Students only",
+//     fees: "₹2000/- per month only",
+//     color: `bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
+//     from-orange-300 to-orange-500`,
+//     text: "text-black"
+// },
+
+// {
+//     name: "Platinum",
+//     students: "1-2 Students only",
+//     fees: "₹20000/- per month only",
+    
+//     text: "text-black"
+// },
 ]
